3b10895d1ec2697da490a1b4456fa4cc51622f3e
Guilherme Oliveira <guilherme.1805.oliveira@gmail.com>
2023-01-30 15:20:58 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+comments ok 
+# fly-1password-secrets
+
+fly-1pasword-secrets is a CLI to sync secrets stored in 1Password with fly applications.
+
+## Requirements
+
+1. 1Password >= `8.9.13` - `brew install --cask 1password`
+2. 1Password CLI >=  `2.13.1` - `brew install --cask 1password-cli`
+3. flyctl `brew install flyctl`
+
+## Getting started
+
+1. Create a Secure Note in 1Password with `fly.{fly-app-name}` in the title. You should not have multiple Secure Notes with this reference.
+
+2. Allow 1Password to connect to 1Password-CLI by going to `Settings` -> `Developer` -> `Command-Line Interface (CLI)` and select `Connect with 1Password CLI`
+
+3. Sign into your fly account
+`flyctl auth login`
+
+## Usage
+
+`python fly-1password-secrets {app-name}`
---
+++ setup.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+from setuptools import setup
+
+long_description = (Path(__file__).parent / "README.md").read_text()
+
+setup(
+    name="fly-1password-secrets",
+    version="0.0.1",
+    description="CLI to sync secrets stored in 1Password with a fly application.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=[], #FIXME:
+    license='MIT',
+    url="https://github.com/significa/fly-1password-secrets",
+    keywords="fly.io, 1password, secrets",
+    author="Significa",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+    ],
+)

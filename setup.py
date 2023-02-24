4bb29c069385ca4c8281d8bd70368f3c50f65c23
Francisco Marques <franciscopcmarques@gmail.com>
2023-02-24 17:01:12 +0000

    dynamic version for setup.py
    Also, normalised quotes

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 import os
+import re
 from pathlib import Path
 
 from setuptools import setup
 
-long_description = (Path(__file__).parent / "README.md").read_text()
-requirements = (Path(__file__).parent / "requirements.txt").read_text().split("\n")
+long_description = (Path(__file__).parent / 'README.md').read_text()
+requirements = (Path(__file__).parent / 'requirements.txt').read_text().split('\n')
 
+version = re.sub(
+    r'^v',
+    '',
+    os.getenv('VERSION', 'v0.0.1')
+)
+
+print(f'Publishing version {version}')
 
 setup(
-    name="1password-secrets",
-    version=os.getenv("VERSION", "0.0.1"),
-    description="1password-secrets is a set of utilities to sync 1Password secrets.",
+    name='1password-secrets',
+    version=version,
+    description='1password-secrets is a set of utilities to sync 1Password secrets.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=requirements,
@@ -20,12 +28,12 @@ setup(
         ],
     },
     license='MIT',
-    url="https://github.com/significa/fly-1password-secrets",
-    keywords="fly.io, 1password, secrets",
-    author="Significa",
+    url='https://github.com/significa/fly-1password-secrets',
+    keywords='fly.io, 1password, secrets',
+    author='Significa',
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
     ],
 )

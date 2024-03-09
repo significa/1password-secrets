01b203e740ad57610409c903ddd9e818f5ee25c2
stefan <stefan@zoof-it.com>
2024-03-09 17:24:44 +0100

    make it work in python3.8

--- onepassword_secrets.py
+++ onepassword_secrets.py
@@ -560,9 +560,8 @@ def main():
     )
     parser.add_argument(
         '--debug',
-        action=argparse.BooleanOptionalAction,
-        type=bool,
-        default=False,
+        action='store_true',
+        # type=bool,
         help='run in debug mode',
     )
 
